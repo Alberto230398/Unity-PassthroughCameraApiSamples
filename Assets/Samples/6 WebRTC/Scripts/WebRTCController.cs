2ee0a1a96c95d3d78705068616a16f273b128d2c
daniele miola <danielemiola96@gmail.com>
2026-03-06 14:32:41 +0100

    Pulizia codice

--- Assets/Samples/6 WebRTC/Scripts/WebRTCController.cs
+++ Assets/Samples/6 WebRTC/Scripts/WebRTCController.cs
@@ -8,44 +8,18 @@ using SimpleWebRTC;
 
 namespace QuestCameraKit.WebRTC {
     public class WebRTCController : MonoBehaviour {
-        [SerializeField] private PassthroughCameraAccess cameraAccess;
-        [SerializeField] private RawImage canvasRawImage;
-        [SerializeField] private GameObject connectionGameObject;
-        [SerializeField] private bool adaptFovToCustomValue;
-        [SerializeField] private float customFovValue;
-        [SerializeField] private Camera[] streamingCameras;
 
 #if WEBRTC_ENABLED
-        private Texture _cameraTexture;
         [SerializeField] private WebRTCConnection _webRTCConnection;
-
-        private IEnumerator Start() {
-            cameraAccess = ResolveCameraAccess(cameraAccess);
-            yield return new WaitUntil(() => cameraAccess && cameraAccess.IsPlaying);
-            if (!cameraAccess) {
-                Debug.LogWarning("[WebRTCController] Passthrough camera unavailable.");
-                yield break;
-            }
-
-            _webRTCConnection = connectionGameObject.GetComponent<WebRTCConnection>();
-            _cameraTexture = cameraAccess.GetTexture();
-            canvasRawImage.texture = _cameraTexture;
-        }
 
         private void Update() {
             if (_webRTCConnection == null) return; // guard
              
             if (OVRInput.GetDown(OVRInput.Button.One)) {
-               _webRTCConnection.StartVideoTransmission();
-                Debug.Log("BUTTON DETECTED");
+                if (!_webRTCConnection.IsVideoTransmissionActive)
+                    _webRTCConnection.StartVideoTransmission();
+                    Debug.Log("BUTTON DETECTED");
             }
-
-            if (adaptFovToCustomValue && streamingCameras[0].fieldOfView != customFovValue) {
-                foreach (var camera in streamingCameras) {
-                    camera.fieldOfView = customFovValue;
-                }
-            }
-
 #if UNITY_EDITOR
             if (Input.GetKeyUp(KeyCode.Space)) {
                 _webRTCConnection.StartVideoTransmission();
@@ -54,13 +28,5 @@ namespace QuestCameraKit.WebRTC {
 #endif
         }
 #endif
-
-        private static PassthroughCameraAccess ResolveCameraAccess(PassthroughCameraAccess configuredAccess) {
-            if (configuredAccess) {
-                return configuredAccess;
-            }
-
-            return FindAnyObjectByType<PassthroughCameraAccess>(FindObjectsInactive.Include);
-        }
     }
 }
