258adbfdb65eba76896925c873d377081fd8feb5
Alberto Merletti <albertomerletti@icloud.com>
2026-06-18 16:51:03 +0200

    Boh raga ora funziona

--- Assets/Scripts/VirtualCamera.cs
+++ Assets/Scripts/VirtualCamera.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VirtualCamera : MonoBehaviour, VideoInterface
 {
     [SerializeField] Camera virtualCamera;
+    public RawImage image;
+    public Material mat;
 
+    public void Start()
+    {  
+    }
     public RenderTexture initVideo(RenderTexture target)
     {
         virtualCamera.targetTexture = target;
+        image.texture = target;
+        mat.color = Color.red;
         return target;
     }
 
