3a3613c63de924e7f0555a2bbaa1b457d3d210e0
Alberto230398 <71700140+Alberto230398@users.noreply.github.com>
2026-06-22 14:53:03 +0200

    Salvagtaggio composite keyframe a bassa qualità

--- Assets/Scripts/KeyFrame Manager.cs
+++ Assets/Scripts/KeyFrame Manager.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyFrameManager : MonoBehaviour
 {
     public Material blendMat;
     public Texture2D keyFrameTexture;
-    private RenderTexture blendedTexture;
+    public RenderTexture blendedTexture;
+
+    public Camera renderCamera;
+
+    public RawImage blendedRawImage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        blendedTexture = new RenderTexture(1920, 1920, 0);
-        blendedTexture.Create();
-
         StartCoroutine(BlendAndSaveKeyFrame());
     }
 
@@ -29,15 +31,26 @@ public class KeyFrameManager : MonoBehaviour
 
 
     void SaveKeyFrame()
-    {
-        string path = Application.dataPath + "/KeyFrames/";
+{
 
-        RenderTexture.active = blendedTexture;
-        Texture2D blendedTexture2D = new Texture2D(blendedTexture.width, blendedTexture.height, TextureFormat.RGB24, false);
-        blendedTexture2D.ReadPixels(new Rect(0, 0, blendedTexture.width, blendedTexture.height), 0, 0);
-        blendedTexture2D.Apply();
+    Debug.Log("Saving Keyframe...");
+    string dir = $"{Application.persistentDataPath}/keyframes/";
+    //string dir = "/Users/albertomerletti/Desktop/keyframes/";
+    System.IO.Directory.CreateDirectory(dir);
 
-        byte[] bytes = blendedTexture2D.EncodeToPNG();
-        System.IO.File.WriteAllBytes(path + "KeyFrame_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png", bytes);
-    }
+    renderCamera.Render();
+    blendedRawImage.texture = blendedTexture;
+
+    // 2. Poi imposta l'active per la lettura
+    Texture2D blendedTexture2D = new Texture2D(blendedTexture.width, blendedTexture.height, TextureFormat.RGBA32, false);
+    RenderTexture.active = blendedTexture;
+    blendedTexture2D.ReadPixels(new Rect(0, 0, blendedTexture.width, blendedTexture.height), 0, 0);
+    blendedTexture2D.Apply();
+
+    // 3. Ripristina sempre l'active (importante!)
+    RenderTexture.active = null;
+
+    byte[] bytes = blendedTexture2D.EncodeToPNG();
+    System.IO.File.WriteAllBytes($"{dir}/KeyFrame_{System.DateTime.Now:yyyyMMdd_HHmmss}.png", bytes);
+}
 }
